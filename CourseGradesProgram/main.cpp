097c37d06eb38e1a25ef6cb6fb397e09b5036494
Emily <Emily.Olsen.Colonthree@outlook.com>
2026-03-03 22:33:08 -0500

    Add files via upload

--- CourseGradesProgram/main.cpp
+++ CourseGradesProgram/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+// struct definition of Student
+struct Student {
+    string name;
+    int id;
+    int* testScores;
+    double averageScore;
+    char letterGrade;
+};
+
+// Function prototypes
+
+Student* getData(string fileName, int& students, int& tests);
+
+void calcAverages(Student* structArr, int studentCount, int testCount);
+
+void getLetterGrades(Student* structArr, int studentCount);
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int studentTotal;
+    int testTotal;
+
+    Student* students;
+
     return 0;
 }
+
+
