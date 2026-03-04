de4876e77651cf029f44ff2e020ae7844c92b51a
Emily <Emily.Olsen.Colonthree@outlook.com>
2026-03-03 23:08:25 -0500

    Implemented getData() function, updated files.
    New getData() function was tested and implemented into program.

--- CourseGradesProgram/main.cpp
+++ CourseGradesProgram/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -25,9 +26,51 @@ int main()
     int studentTotal;
     int testTotal;
 
-    Student* students;
+    Student* students = getData("student_data.txt", studentTotal, testTotal);
 
     return 0;
+}
+
+Student* getData(string fileName, int& students, int& tests)
+{
+    fstream dataFile(fileName);
+
+    Student* studentArray;
+
+    if (dataFile)
+    {
+        string line;
+
+        // Gets line and stringstream from first line of text file
+        getline(dataFile, line);
+        stringstream ss(line);
+
+        ss >> students >> tests; // Takes number of students and tests from the header
+
+        studentArray = new Student[students];
+
+        for (int i = 0; i < students; i++) // For all the students
+        {
+            getline(dataFile, line); // Gets a new line
+            stringstream ss(line);
+
+            ss >> studentArray[i].name >> studentArray[i].id;
+
+            studentArray[i].testScores = new int[tests];
+
+            for (int j = 0; j < tests; j++) // For all the test scores of i student
+            {
+                ss >> studentArray[i].testScores[j];
+            }
+        }
+    }
+    else
+    {
+        cout << "ERROR: File could not be opened.\n";
+        exit(1);
+    }
+
+    return studentArray;
 }
 
 
